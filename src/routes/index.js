8a7b46f20e682eba0f7118b4d55dbb901a842a0e
E1P <ewan.pearce@gmail.com>
2022-03-30 16:46:21 +0100

    Retrieve env.MODE in get() and display in homepage.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 export async function get(request) {
 	console.log('Getting...', request);
 	const environment = import.meta.env.VITE_TEST_VARIABLE;
+	const mode = import.meta.env.MODE;
 	return {
-		body: { environment }
+		body: { environment, mode }
 	};
 }
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,10 @@
 <script>
 	// $: environment = import.meta.env.VITE_TEST_VARIABLE;
 	export let environment;
+	export let mode;
 </script>
 
 <h1>Vercel Env Test</h1>
 
-<h2>Current environment = {environment}</h2>
+<h2>Current environment (VITE_TEST_VARIABLE) = {environment}</h2>
+<h2>Current mode (import.meta.env.MODE)) = {mode}</h2>
